5cd57cc96ccb0f0293c627c2a249963c2790833c
Rick Huff <rhuff2@yahoo.com>
2022-11-24 06:56:08 -0600

    Daily commit.

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
-const MaterialComponents = [MatButtonModule];
+const MaterialComponents = [
+  MatButtonModule,
+  MatIconModule,
+  MatDividerModule,
+];
 
 @NgModule({
   imports: [MaterialComponents],
